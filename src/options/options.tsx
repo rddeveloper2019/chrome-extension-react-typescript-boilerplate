d59d744c262a30c1c056c9baf768e97e0f3b0f78
Ridvan Dzhalil <fatzey2010@gmail.com>
2022-03-14 19:08:38 +0300

    @types

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './options.css';
 
-const test = <p>Hello World</p>;
+const test = (
+  <div>
+    <p>Hello World</p>
+    <img src='icon.png' alt='icon' />
+  </div>
+);
 const root = document.createElement('div');
 document.body.appendChild(root);
 
